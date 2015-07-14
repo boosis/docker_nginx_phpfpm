a49f6a514149fd3fd1a8c6e0bd8937adee9a90b1
Bill Yildirim <bill@dyln.io>
2015-07-14 16:04:30 +0100

    Fix sed path

--- nginx.sh
+++ nginx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-/usr/bin/sed -i "s/{{NGINX_SERVER_NAME}}/${NGINX_SERVER_NAME}/" /etc/nginx/sites-enabled/default
-/usr/bin/sed -i "s/{{NGINX_ROOT}}/${NGINX_ROOT}/" /etc/nginx/sites-enabled/default
+/bin/sed -i "s/{{NGINX_SERVER_NAME}}/${NGINX_SERVER_NAME}/" /etc/nginx/sites-enabled/default
+/bin/sed -i "s/{{NGINX_ROOT}}/${NGINX_ROOT}/" /etc/nginx/sites-enabled/default
 
 nginx
