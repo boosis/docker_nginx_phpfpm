9f20736f82f72e0630864a1a2138c2a9fcae2a04
Bill Yildirim <bill@dyln.io>
2015-08-26 09:06:25 +0100

    Fix nginx sed

--- nginx.sh
+++ nginx.sh
@@ -4,8 +4,8 @@
 [ -z "$NGINX_ROOT" ] && echo "Need to set NGINX_ROOT" && exit 1;
 [ -z "$APPLICATION_ENV" ] && echo "Need to set APPLICATION_ENV" && exit 1;
 
-sed -i "s#{{NGINX_SERVER_NAME}}#${NGINX_SERVER_NAME}#g" /etc/nginx/conf.d/default
-sed -i "s#{{NGINX_ROOT}}#${NGINX_ROOT}#g" /etc/nginx/conf.d/default
-sed -i "s#{{APPLICATION_ENV}}#${APPLICATION_ENV}#g" /etc/nginx/conf.d/default
+sed -i "s#{{NGINX_SERVER_NAME}}#${NGINX_SERVER_NAME}#g" /etc/nginx/sites-enabled/default
+sed -i "s#{{NGINX_ROOT}}#${NGINX_ROOT}#g" /etc/nginx/sites-enabled/default
+sed -i "s#{{APPLICATION_ENV}}#${APPLICATION_ENV}#g" /etc/nginx/sites-enabled/default
 
 nginx
