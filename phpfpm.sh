e037f88ebc8dff456c0606c035e701fd547e4a10
Bill Yildirim <bill@dyln.io>
2016-06-22 13:22:55 +0100

    php 5.6 rpo

--- phpfpm.sh
+++ phpfpm.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+[ -z "$XDEBUG_REMOTE_HOST" ] && echo "Need to set XDEBUG_REMOTE_HOST" && exit 1;
+
+sed -i "s#{{XDEBUG_REMOTE_HOST}}#${XDEBUG_REMOTE_HOST}#g" /etc/php/5.6/mods-available/xdebug.ini
+
 php-fpm5.6 -c /etc/php/5.6/fpm
